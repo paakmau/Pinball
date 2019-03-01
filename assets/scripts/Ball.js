6cd3cbcf1463a2cbb024140f8589d0f428ca926b
Bomou He <paakmau@outlook.com>
2019-03-01 17:22:16 +0800

    实现了小球Bomb, 蹦床组件

--- assets/scripts/Ball.js
+++ assets/scripts/Ball.js
@@ -7,12 +7,32 @@ var Ball = cc.Class({
 
     // LIFE-CYCLE CALLBACKS:
 
-    onLoad () {
+    onLoad() {
+        // 获取成员变量
         this.rigidBody = this.getComponent(cc.RigidBody);
         this.worldCenter = this.rigidBody.getWorldCenter();
-    }
 
-    // update (dt) {},
+        // 监听Bomb事件
+        this.node.on("ball_bomb", this.bomb, this);
+
+
+        // TODO: 用于测试, 注册键盘输入
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    },
+
+    bomb(dir) {
+        this.rigidBody.applyLinearImpulse(dir, this.worldCenter);
+    },
+
+    // TODO: 用于测试, KeyDown回调
+    // 按下空格, 发射小破球
+    onKeyDown(event) {
+        switch(event.keyCode) {
+            case cc.macro.KEY.space:
+                this.bomb(cc.v2(0, 10000));
+                break;
+        }
+    }
 });
 
 module.exports = Ball;
--- assets/scripts/Trampoline.js
+++ assets/scripts/Trampoline.js
@@ -1,9 +1,15 @@
 
+/**
+ * 蹦床组件
+ * 该Node的Collider被小球触到后
+ * 会生成一个Node的y向量方向, 大小为bombPower的冲量
+ * 并广播
+ */
 var Trampoline = cc.Class({
     extends: cc.Component,
 
     properties: {
-        bombPower: 5
+        bombPower: 100
     },
 
     // LIFE-CYCLE CALLBACKS:
@@ -12,10 +18,10 @@ var Trampoline = cc.Class({
         this.bombDir = cc.v2(0, 1).rotate(this.node.rotation).mul(this.bombPower);
     },
     onBeginContact(contact, selfCollider, otherCollider) {
-
+        // 若被小球碰到, 广播Bomb事件
+        if(otherCollider.node.name == "Ball")
+            this.node.emit("ball_bomb", this.bombDir);
     }
-
-    // update (dt) {},
 });
 
 
