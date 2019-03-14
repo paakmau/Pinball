b45eb9970488e64f65352646161f810d3298a9ad
Bomou He <paakmau@outlook.com>
2019-03-14 20:46:30 +0800

    修改了黑洞

--- assets/scripts/GameLogic/Ball.js
+++ assets/scripts/GameLogic/Ball.js
@@ -26,14 +26,15 @@ var Ball = cc.Class({
         this.node.position = pos;
         this.rigidBody.linearVelocity = cc.v2(0, 0);
         // TODO: 应当消失
+        this.originColor = this.node.color;
         this.node.color = new cc.Color(50, 50, 50);
     },
 
     goOutOfBlackHole(pos, bombDir) {
         this.node.position = pos;
         this.rigidBody.applyLinearImpulse(bombDir, this.worldCenter);
-        // TODO: 应当跟随Into改变
-        this.node.color = new cc.Color(255, 255, 255);
+        // TODO: 等美工
+        this.node.color = this.originColor;
     },
 
     restart() {
