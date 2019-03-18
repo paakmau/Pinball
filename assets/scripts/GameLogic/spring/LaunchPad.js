21da47c63e64675405af46ed4df70bca9984d281
NJUleo <1250925329@qq.com>
2019-03-18 23:02:03 +0800

    spring

--- assets/scripts/GameLogic/spring/LaunchPad.js
+++ assets/scripts/GameLogic/spring/LaunchPad.js
@@ -1,16 +1,20 @@
+var BallBombTrampolineGLEvent = require("../../Message/GameLogic/BallBombTrampolineGLEvent")
 cc.Class({
     extends: cc.Component,
     properties: {
+        bombPower: 2000
     },
     onLoad(){
         this.enable = true;
         var that = this;
+        this.firstPosition = this.node.position;
+        this.endPosition = this.node.position;
 
         // this.node.on(cc.Node.EventType.MOUSE_DOWN, this.springStart, this);
         cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.springDown, this);
         cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.springUp, this);
         this.rigidBody = this.getComponent(cc.RigidBody);
-        this.rigidBody.enabledContactListener = false;
+        // this.rigidBody.enabledContactListener = false;
         
     },
     springDown(event){
@@ -23,11 +27,22 @@ cc.Class({
     },
     springUp(event){
         cc.log("spring up");
-        this.rigidBody.enabledContactListener = true;
         this.node.stopAllActions();
         var action = cc.moveTo(0.3, cc.v2(252.7,-488.4));
         this.node.runAction(action);
         this.enable = true;
+        this.endPosition = this.node.position;
+        cc.log("end = " + this.endPosition.y + " and first = " + this.firstPosition);
+    },
+
+    onBeginContact(contact, selfCollider, otherCollider) {
+        // 触发Bomb事件
+        var bombDir = this.firstPosition.sub(this.endPosition).normalize().mul(this.bombPower).mul(this.firstPosition.y - this.endPosition.y);
+        var bombEvent = new BallBombTrampolineGLEvent();
+        bombEvent.init(bombDir, BallBombTrampolineGLEvent.TrampolineType.CircleTrampoline);
+        this.node.dispatchEvent(bombEvent);
+        cc.log(bombDir.x + "and" + bombDir.y);
+        this.endPosition = this.firstPosition;
     }
 
 });
