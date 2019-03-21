ffded14758d77582e31b8599dc754c7e1756b273
Bomou He <paakmau@outlook.com>
2019-03-21 23:48:28 +0800

    RegisterOrLoginByWxId测试通过

--- assets/scripts/Api/ApiTest.js
+++ assets/scripts/Api/ApiTest.js
@@ -6,30 +6,14 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+        loginEditBox: {
+            type: cc.EditBox,
+            default: null
+        },
+        updateUserEditBox: cc.EditBox
     },
-
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {},
-
-    start () {
-
-    },
-
-    // update (dt) {},
+    
+    onClickLogin() {
+        UserApi.RegisterOrLoginByWxId(this.loginEditBox.string, res=>{ console.log(res) })
+    }
 });
