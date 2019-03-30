a4614a6ae0d7f9428906948a889426a3db035569
NJUleo <1250925329@qq.com>
2019-03-30 23:35:07 +0800

    转盘亮灯

--- assets/scripts/GameLogic/CentralCircle/CentralCircleLights.js
+++ assets/scripts/GameLogic/CentralCircle/CentralCircleLights.js
@@ -3,10 +3,23 @@ var CentralCircleLights = cc.Class({
     extends: cc.Component,
 
     properties: {
+        Lights :{
+            default: [],
+            type: cc.Animation,
+        }
+    },
+    onLoad(){
+        this.setValue(0, 0);
     },
 
     setValue(value, mxValue) {
-        this.node.rotation = value/mxValue * 360
+        //this.node.rotation = value/mxValue * 360
+        for(var i = 0; i <= value && i < this.Lights.length; i++){
+            this.Lights[i].play("LightOn");
+        }
+        for(var i = value + 1; i < this.Lights.length; i++){
+            this.Lights[i].play("LightOff");
+        }
     }
 });
 
